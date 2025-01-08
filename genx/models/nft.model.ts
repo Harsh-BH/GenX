efc20f9cf8d36a6539fadd445698cdb88d2af9ad
Aksh10x <akshathsurwase@gmail.com>
2025-01-08 15:58:58 +0530

    nft model

--- genx/models/nft.model.ts
+++ genx/models/nft.model.ts
@@ -1,11 +1,34 @@
 import mongoose, { Schema } from "mongoose";
 
 export interface NFT {
-    
+    name: string;
+    image: string;
+    description: string;
+    recipientAddress: string;
+    tokenURI: string;
 }
 
 const nftSchema: Schema<NFT> = new mongoose.Schema({
-
+    name: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    recipientAddress: {
+        type: String,
+        required: true
+    },
+    tokenURI: {
+        type: String,
+        required: true
+    }
 })
 
 export const nftModel = (mongoose.models.Nft as mongoose.Model<NFT>) || mongoose.model<NFT>("Nft", nftSchema)
